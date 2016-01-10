5067fbd1faff28c78be73e17aba2b70ca52bd56b
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2016-01-11 02:57:05 +0900

    remove dupe dirs

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var browserSync = require('browser-sync');
 var cp          = require('child_process');
 var harp        = require('harp')
 
-
 gulp.task('serve', function (done) {
   harp.server('.', {
     port: 9000
@@ -11,11 +10,11 @@ gulp.task('serve', function (done) {
 });
 
 gulp.task('build', function (done) {
-  cp.exec('harp compile _src dist', {stdio: 'inherit'}).on('close', done)
+  cp.exec('harp compile _src dist')
 });
 
 gulp.task('watch', function () {
-  gulp.watch("public/**/*.{jade,styl,haml,sass,scss,less}", browserSync.reload)
+  gulp.watch("/**/*.{jade,styl,haml,sass,scss,less}", browserSync.reload)
 });
 
 gulp.task('browser-sync', function() {
