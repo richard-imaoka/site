22525228a6ed9ff9956f73946d18743027225de4
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2016-01-14 00:10:25 +0900

    first article in draft

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,8 @@ gulp.task('build', function (done) {
 });
 
 gulp.task('watch', function () {
-gulp.task('watch', function () {
-  gulp.watch("_src/**/*.{jade,styl,haml,sass,scss,less,md,ejs,json}", browserSync.reload);
-  gulp.watch("_src/**/*.{jade,styl,haml,sass,scss,less,md,ejs,json}", ['build']);
+  gulp.watch("_src/**/*.{jade,styl,haml,sass,scss,less,md,ejs,json,html,css,js}", browserSync.reload);
+  //gulp.watch("_src/**/*.{jade,styl,haml,sass,scss,less,md,ejs,json}", ['build']);
 });
 
 gulp.task('browser-sync', function() {
